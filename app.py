53ad31722ebb394476e7670d5ba64600f6806db1
Richie Zeng <richzeng@gmail.com>
2014-04-06 10:14:42 -0700

    URL Routing

--- app.py
+++ app.py
@@ -5,4 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return 'Hello World!'
+    return 'This is the main site'
+
+@app.route('/<referral>')
+def main_special(referral):
+    return 'This is the page for people with the %s code' % referral
