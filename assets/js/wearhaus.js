d9e7ccabe2017bc2447177d8bfe0241473a68bf5
Richie Zeng <richzeng@gmail.com>
2014-04-20 22:01:25 -0700

    Fixed transitions in shortcuts

---
+++ assets/js/wearhaus.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+	/*$("#shortcuts li div.home").hover(function(){
+		console.log("hello");
+		$("#shortcuts li div.home").css("background-color", "#00bccc");
+		$("div.icon-home").css("background-image", "url('../assets/img/home-black-fill.png')");
+	}, function(){
+		$("#shortcuts li div.home").css("background-color", "transparent");
+		$("div.icon-home").css("background-image", "url('../assets/img/home-blue-outline.png')");
+	});*/
+
+	$("#shortcuts li div.social").hover(function(){
+		console.log("hello");
+		$("#shortcuts li div.social").css("background-color", "#00bccc");
+		$("div.icon-social").css("background-image", "url('../assets/img/social-black-fill.png')");
+	}, function(){
+		$("#shortcuts li div.social").css("background-color", "transparent");
+		$("div.icon-social").css("background-image", "url('../assets/img/social-blue-outline.png')");
+	});
+
+
+});
