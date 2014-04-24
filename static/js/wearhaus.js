428a3b9b04936214e9438717da67a1601a02a5f2
Richie Zeng <richzeng@gmail.com>
2014-04-24 16:14:43 -0700

    Made icon active on click

--- static/js/wearhaus.js
+++ static/js/wearhaus.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 
+	/* Shortcut hover */
+
 	$("div.home").hover(function(){
 		$("p.home").css("color", "white");
 	}, function(){
@@ -36,12 +38,55 @@ $(document).ready(function(){
 		$("p.story").css("color", "none");
 	});
 
+	/* Shortcut click */
+
+	var currentActiveShortcut = null;
+
+	function makeShortcutActive(shortcut) {
+		if (currentActiveShortcut) {
+			$("div.inner." + currentActiveShortcut).css(
+				"background-image", "url('../static/img/" + currentActiveShortcut + "-blue-outline.png')"
+			);
+		}
+		$("div.inner." + shortcut).css(
+			"background-image", "url('../static/img/" + shortcut + "-blue-fill.png')"
+		);
+		currentActiveShortcut = shortcut;
+	}
+
+	$("div.inner.home").click(function() {
+		makeShortcutActive("home");
+	});
+
+	$("div.inner.social").click(function() {
+		makeShortcutActive("social");
+	});
+
+	$("div.inner.customize").click(function() {
+		makeShortcutActive("customize");
+	});
+
+	$("div.inner.interface").click(function() {
+		makeShortcutActive("interface");
+	});
+
+	$("div.inner.details").click(function() {
+		makeShortcutActive("details");
+	});
+
+	$("div.inner.story").click(function() {
+		makeShortcutActive("story");
+	});
+
+
+	/* Play button hover */
+
 	$("#play-button").hover(function() {
 		console.log("hello");
-		$("#play-button .inner").css("background-color", "#00bccc");
+		$("#play-button .inner-play").css("background-color", "#00bccc");
 		$("#play-symbol").css("border-left-color", "white");
 	}, function(){
-		$("#play-button .inner").css("background-color", "none");
+		$("#play-button .inner-play").css("background-color", "none");
 		$("#play-symbol").css("border-left-color", "#00bccc");
 
 	});
