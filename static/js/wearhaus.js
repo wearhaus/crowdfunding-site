a097fced936b7b9a286476169b426a70f79c30dc
Richie Zeng <richzeng@gmail.com>
2014-04-28 04:48:47 -0700

    Smooth scrolling to target when clicking on shortcut icon

--- static/js/wearhaus.js
+++ static/js/wearhaus.js
@@ -88,7 +88,21 @@ $(document).ready(function() {
 		}
 	});
 
+	/* Smooth transition on shortcut click */
+	$(document).ready(function(){
+		$('a[href^="#"]').on('click',function (e) {
+		    e.preventDefault();
 
+		    var target = this.hash,
+		    $target = $(target);
+
+		    $('html, body').stop().animate({
+		        'scrollTop': $target.offset().top - 100
+		    }, 500, 'swing', function () {
+		        window.location.hash = target;
+		    });
+		});
+	});
 
 	/* Play button hover */
 
