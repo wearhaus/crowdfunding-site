8bcad4bc4d2f6c5f78e26f4ed3ec4995a2ca9970
Richie Zeng <richzeng@gmail.com>
2014-04-30 03:39:09 -0700

    Added SQLAlchemy for postgres stuff

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import os
 from flask import Flask, render_template
+from flask.ext.sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db = SQLAlchemy(app)
 
 @app.route('/')
 def main():
