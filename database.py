2c497afb84e3c8ee7720537504b4824b7b0eef3e
Richie Zeng <richzeng@gmail.com>
2014-05-05 14:53:50 -0700

    Preparing to load 5 promo codes

--- database.py
+++ database.py
@@ -10,6 +10,31 @@ def create_user(email, code):
         db.session.rollback()
         print "Aborted create_user"
 
+def clear_users():
+    for user in User.query.all():
+        try:
+            print "Deleting " + user.email
+            db.session.delete(user)
+            db.session.commit()
+        except Exception as e:
+            print e
+            db.session.rollback()
+            print "Aborted delete user"
+    print User.query.all()
+
+def clear_referral_codes():
+    all_codes = ReferralCode.query.all()
+    for code in all_codes:
+        try:
+            print "Deleting " + code.referral_code
+            db.session.delete(code)
+            db.session.commit()
+        except Exception as e:
+            print e
+            db.session.rollback()
+            print "Aborted delete code"
+    print ReferralCode.query.all()
+
 def add_referral_code(code):
     referral_code = ReferralCode(code)
     try:
@@ -21,6 +46,7 @@ def add_referral_code(code):
 
 def load_referral_codes(path):
     with open(path, 'r') as f:
+        print f
         code = f.readline()[:-1]
         while code:
             print code
